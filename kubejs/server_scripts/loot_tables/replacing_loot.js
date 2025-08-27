1fe944f1910e8fe44a9c43e17b8792ef753fbcdd
Kisuny <homeca19@gmail.com>
2025-08-28 00:35:04 +0300

    removed replacing some loot

--- kubejs/server_scripts/loot_tables/replacing_loot.js
+++ kubejs/server_scripts/loot_tables/replacing_loot.js
@@ -1,12 +1,12 @@
 LootJS.modifiers(event => {
 
     const items_for_replace = [
-        { item: "minecraft:iron_ingot", replace_item: "minecraft:raw_iron" },
-        { item: "minecraft:gold_ingot", replace_item: "minecraft:raw_gold" },
-        { item: "minecraft:gold_block", replace_item: "minecraft:raw_gold_block" },
-        { item: "minecraft:iron_block", replace_item: "minecraft:raw_iron_block" },
-        { item: "minecraft:copper_ingot", replace_item: "minecraft:raw_copper" },
-        { item: "minecraft:copper_block", replace_item: "minecraft:raw_copper_block" },
+        // { item: "minecraft:iron_ingot", replace_item: "minecraft:raw_iron" },
+        // { item: "minecraft:gold_ingot", replace_item: "minecraft:raw_gold" },
+        // { item: "minecraft:gold_block", replace_item: "minecraft:raw_gold_block" },
+        // { item: "minecraft:iron_block", replace_item: "minecraft:raw_iron_block" },
+        // { item: "minecraft:copper_ingot", replace_item: "minecraft:raw_copper" },
+        // { item: "minecraft:copper_block", replace_item: "minecraft:raw_copper_block" },
     ]
 
     items_for_replace.forEach(item => {
