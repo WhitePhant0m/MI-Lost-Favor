76d169cb0c388d4a751880bb9952bb1f570c8e8b
Kisuny <homeca19@gmail.com>
2025-09-01 00:48:11 +0300

    Added function "customPedestalCraft"

--- kubejs/server_scripts/mods/pastel/pedestal.js
+++ kubejs/server_scripts/mods/pastel/pedestal.js
@@ -1,14 +1,40 @@
+// basic: Cyan, Magenta and Yellow pedestal variants
+// simple: CMY pedestal variant with matching structure
+// advanced: Onyx pedestal variant with matching structure
+// complex: Moonstone pedestal variant with matching structure
+
+const customPedestalCraft = (event, args) => {
+  event.custom({
+    "type": "pastel:pedestal",
+    "time": args.time,
+    "tier": args.tier || "basic",
+    "colors": {
+      "pastel:magenta": args.amethyst,
+      "pastel:yellow": args.citrine,
+      "pastel:cyan": args.topaz,
+      "pastel:black": args.onyx,
+      "pastel:white": args.moonstone
+    },
+    "experience": args.experience || 0.0,
+    "pattern": args.pattern,
+    "key": args.keys,
+    "result": args.result,
+    "required_advancement": args.advancement,
+    "disable_yield_upgrades": args.yield_upgrades || false,
+  });
+};
+
 ServerEvents.recipes(event => {
 
-    var tier1bp = 'Mysterious Blueprint'
-    var tier2bp = "Storage Blueprint"
-    var tier3bp = "Automation Blueprint"
-    var tier4bp = "Quantum Blueprint"
-    var tier5bp = "Divine Blueprint"
+  var tier1bp = 'Mysterious Blueprint'
+  var tier2bp = "Storage Blueprint"
+  var tier3bp = "Automation Blueprint"
+  var tier4bp = "Quantum Blueprint"
+  var tier5bp = "Divine Blueprint"
 
-    
 
-  function blueprint(input, bpTier , color) {
+
+  function blueprint(input, bpTier, color) {
 
     event.custom({
       "type": "pastel:pedestal",
@@ -32,7 +58,7 @@ ServerEvents.recipes(event => {
         "B": input,
       },
       "result": {
-        "components": { "immersiveengineering:blueprint": bpTier, "minecraft:item_name" : "{'text':'" + bpTier + "','color':'" + color + "'}"},
+        "components": { "immersiveengineering:blueprint": bpTier, "minecraft:item_name": "{'text':'" + bpTier + "','color':'" + color + "'}" },
         "id": "immersiveengineering:blueprint",
         "count": 1
 
@@ -41,11 +67,11 @@ ServerEvents.recipes(event => {
     })
   }
 
-  blueprint("kubejs:mysterious_blueprint", tier1bp , '#84b9ff')
-  blueprint("kubejs:storage_blueprint", tier2bp , '#fff678')
-  blueprint("kubejs:automation_blueprint", tier3bp , '#8de8ff')
-  blueprint("kubejs:quantum_blueprint", tier4bp , '#c795ff')
-  blueprint("kubejs:divine_blueprint", tier5bp , '#abffc0')
+  blueprint("kubejs:mysterious_blueprint", tier1bp, '#84b9ff')
+  blueprint("kubejs:storage_blueprint", tier2bp, '#fff678')
+  blueprint("kubejs:automation_blueprint", tier3bp, '#8de8ff')
+  blueprint("kubejs:quantum_blueprint", tier4bp, '#c795ff')
+  blueprint("kubejs:divine_blueprint", tier5bp, '#abffc0')
 
 
 })
