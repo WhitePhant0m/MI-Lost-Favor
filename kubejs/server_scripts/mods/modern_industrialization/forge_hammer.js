c1cda4371957b1da6eaba4a2353f3536491b6d7a
Kisuny <homeca19@gmail.com>
2025-10-03 12:53:28 +0300

    minor changes

--- kubejs/server_scripts/mods/modern_industrialization/forge_hammer.js
+++ kubejs/server_scripts/mods/modern_industrialization/forge_hammer.js
@@ -17,5 +17,5 @@ ServerEvents.recipes(event => {
     });
 
 
-    event.recipes.modern_industrialization.forge_hammer('minecraft:iron_ingot', 'ytech:iron_bloom', 20, 1)
+    event.recipes.modern_industrialization.forge_hammer('minecraft:iron_ingot', 'ytech:iron_bloom', 5, 1)
 })
