8bf438f16319c3381806925b4ecb40f53bddfd3b
Kisuny <homeca19@gmail.com>
2025-10-04 20:35:24 +0300

    Added tags for broom quest

---
+++ kubejs/server_scripts/mods/hexerei/tags.js
@@ -0,0 +1,7 @@
+ServerEvents.tags('item', event => {
+    event.add("hexerei:brooms", [
+        'hexerei:witch_hazel_broom', 
+        'hexerei:willow_broom', 
+        'hexerei:mahogany_broom',
+    ])
+})
