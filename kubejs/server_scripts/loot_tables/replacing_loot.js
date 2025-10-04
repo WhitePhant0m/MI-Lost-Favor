908c65ff4c4e1ff9430d8135696ba3c65f73c51c
Kisuny <homeca19@gmail.com>
2025-10-04 20:35:39 +0300

    rework replacing loot

--- kubejs/server_scripts/loot_tables/replacing_loot.js
+++ kubejs/server_scripts/loot_tables/replacing_loot.js
@@ -1,22 +1,24 @@
 LootJS.modifiers(event => {
 
     const items_for_replace = [
-        // { item: "minecraft:iron_ingot", replace_item: "minecraft:raw_iron" },
-        // { item: "minecraft:gold_ingot", replace_item: "minecraft:raw_gold" },
-        // { item: "minecraft:gold_block", replace_item: "minecraft:raw_gold_block" },
-        // { item: "minecraft:iron_block", replace_item: "minecraft:raw_iron_block" },
-        // { item: "minecraft:copper_ingot", replace_item: "minecraft:raw_copper" },
-        // { item: "minecraft:copper_block", replace_item: "minecraft:raw_copper_block" },
+        { item: "minecraft:iron_ingot", replace_item: "minecraft:raw_iron" },
+        { item: "minecraft:gold_ingot", replace_item: "minecraft:raw_gold" },
+        { item: "minecraft:gold_block", replace_item: "minecraft:raw_gold_block" },
+        { item: "minecraft:iron_block", replace_item: "minecraft:raw_iron_block" },
+        { item: "minecraft:copper_ingot", replace_item: "minecraft:raw_copper" },
+        { item: "minecraft:copper_block", replace_item: "minecraft:raw_copper_block" },
+        { item: "minecraft:leather", replace_item: "ytech:raw_hide" },
     ]
 
-    items_for_replace.forEach(item => {
-        event
-            .addTableModifier(/.*chests.*/)
-            .replaceLoot(item.item, item.replace_item, true)
+    const allowed_types_for_replacing = [LootType.CHEST, LootType.ENTITY, LootType.FISHING, LootType.ARCHAEOLOGY, LootType.VAULT, LootType.GIFT, LootType.PIGLIN_BARTER, LootType.GENERIC]
 
-        event
-            .addTableModifier(/.*entities.*/)
+    items_for_replace.forEach(item => {
+        allowed_types_for_replacing.forEach(type => {
+            event
+            .addTableModifier(type)
             .replaceLoot(item.item, item.replace_item, true)
+        });
     });
 
 })
+
