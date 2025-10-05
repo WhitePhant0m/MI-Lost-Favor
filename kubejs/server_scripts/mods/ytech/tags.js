701c084be676261ac1c487826296b5cb469adadd
Kisuny <homeca19@gmail.com>
2025-10-05 23:08:46 +0300

    Added minecraft:mineable/pickaxe for ytech:aqueducts

--- kubejs/server_scripts/mods/ytech/tags.js
+++ kubejs/server_scripts/mods/ytech/tags.js
@@ -1,40 +1,46 @@
-ServerEvents.tags('item', event => {
-        event.removeAllTagsFrom([
-            'ytech:tin_plate', 
-            'ytech:lead_plate', 
-            'ytech:copper_plate', 
-            'ytech:golden_plate', 
-            'ytech:bronze_plate', 
-            'ytech:iron_plate',
-            'ytech:lead_ingot', 
-            'ytech:tin_ingot', 
-            'ytech:bronze_ingot',
-            'ytech:bronze_rod', 
-            'ytech:tin_rod', 
-            'ytech:lead_rod', 
-            'ytech:copper_rod', 
-            'ytech:golden_rod', 
-            'ytech:iron_rod',
-            'ytech:golden_bolt', 
-            'ytech:iron_bolt', 
-            'ytech:bronze_bolt', 
-            'ytech:copper_bolt', 
-            'ytech:tin_bolt', 
-            'ytech:lead_bolt',
-            'ytech:bronze_block',
-            'ytech:tin_block',
-            
-        ])
+ServerEvents.tags("item", event => {
+    event.removeAllTagsFrom([
+        "ytech:tin_plate",
+        "ytech:lead_plate",
+        "ytech:copper_plate",
+        "ytech:golden_plate",
+        "ytech:bronze_plate",
+        "ytech:iron_plate",
+        "ytech:lead_ingot",
+        "ytech:tin_ingot",
+        "ytech:bronze_ingot",
+        "ytech:bronze_rod",
+        "ytech:tin_rod",
+        "ytech:lead_rod",
+        "ytech:copper_rod",
+        "ytech:golden_rod",
+        "ytech:iron_rod",
+        "ytech:golden_bolt",
+        "ytech:iron_bolt",
+        "ytech:bronze_bolt",
+        "ytech:copper_bolt",
+        "ytech:tin_bolt",
+        "ytech:lead_bolt",
+        "ytech:bronze_block",
+        "ytech:tin_block",
 
-        event.add('modern_industrialization:forge_hammer_tools', [
-            'ytech:bronze_hammer', 
-            'ytech:lead_hammer', 
-            'ytech:copper_hammer', 
-            'ytech:iron_hammer', 
-            'ytech:iron_hammer', 
-            'ytech:stone_hammer', 
-            'ytech:golden_hammer', 
-            'ytech:tin_hammer'
-        ])
+    ])
+
+    event.add("modern_industrialization:forge_hammer_tools", [
+        "ytech:bronze_hammer",
+        "ytech:lead_hammer",
+        "ytech:copper_hammer",
+        "ytech:iron_hammer",
+        "ytech:iron_hammer",
+        "ytech:stone_hammer",
+        "ytech:golden_hammer",
+        "ytech:tin_hammer"
+    ])
+
+})
+
+ServerEvents.tags("block", event => {
+    event.add("minecraft:mineable/pickaxe", "#ytech:aqueducts")
+
 
 })
