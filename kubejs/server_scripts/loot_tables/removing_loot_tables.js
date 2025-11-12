8cc1e883db78babcb35498f07cdd5ad77a785c6a
Kisuny <homeca19@gmail.com>
2025-11-12 05:12:15 +0300

    Removed hero_of_the_village loot tables

--- kubejs/server_scripts/loot_tables/removing_loot_tables.js
+++ kubejs/server_scripts/loot_tables/removing_loot_tables.js
@@ -2,6 +2,11 @@ LootJS.modifiers(event => {
     const remove_loot_tables = [
         "immersiveengineering:chests/engineers_house",
         "immersiveengineering:gameplay/hero_of_the_village/machinist",
+        "immersiveengineering:gameplay/hero_of_the_village/engineer",
+        "immersiveengineering:gameplay/hero_of_the_village/electrician",
+        "immersiveengineering:gameplay/hero_of_the_village/gunsmith",
+        "immersiveengineering:gameplay/hero_of_the_village/outfitter",
+        "ae2:gameplay/hero_of_the_village/fluix_researcher_gifts",
         "modern_industrialization:gameplay/hero_of_the_village/industrialist_gift",
         "hexerei:chests/courier_package",
     ]
