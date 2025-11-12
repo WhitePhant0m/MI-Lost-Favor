2337715bb58125f323f46eaf741d81df879badcb
saeta <saeta_@outlook.com>
2025-11-12 20:28:53 +0700

    treated stick recipe + resource pack update

--- kubejs/server_scripts/mods/modern_industrialization/machines/cutting_machine.js
+++ kubejs/server_scripts/mods/modern_industrialization/machines/cutting_machine.js
@@ -1,5 +1,12 @@
 ServerEvents.recipes(event => {
-        event.recipes.modern_industrialization.cutting_machine(8, 100)
-        .itemIn("kubejs:tempered_glass")
-        .itemOut("kubejs:lens")
+    event.recipes.modern_industrialization.cutting_machine(8, 100)
+    .itemIn("kubejs:tempered_glass")
+    .itemOut("kubejs:lens")
+
+    miMachineCraft(event, {energy:4, time:100, machine:"modern_industrialization:cutting_machine",
+        inputFluids:[[{fluid:"modern_industrialization:lubricant"}, 1]],
+        inputItems:[[{tag:"immersiveengineering:treated_wood"}],],
+        outputItems:[[{item:"immersiveengineering:stick_treated"}, 3]]
+    })
 })
+
