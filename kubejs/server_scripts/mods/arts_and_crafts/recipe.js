a956a0ed046c85aa88f5c7cdf7d2a58bab9e13a7
Kisuny <homeca19@gmail.com>
2025-11-18 20:48:44 +0300

    Ytech style craft for all beds

---
+++ kubejs/server_scripts/mods/arts_and_crafts/recipe.js
@@ -0,0 +1,19 @@
+ServerEvents.recipes(event => {
+
+    yTechShaped(event, {
+        pattern: [
+            "# @",
+            "WWW",
+            "PPP"
+        ],
+        key: {
+            "#": { "tag": "c:hammers" },
+            "P": { "tag": "minecraft:wooden_slabs" },
+            "@": { "tag": "c:saws" },
+            "W": { "item": "arts_and_crafts:bleached_wool" }
+        },
+        outputItems: [[{ id: "arts_and_crafts:bleached_bed" }, 1]],
+        removeRecipeType: "minecraft:crafting_shaped"
+    })
+
+})
--- kubejs/server_scripts/mods/eternal_starlight/eternal_starlight_recipes.js
+++ kubejs/server_scripts/mods/eternal_starlight/eternal_starlight_recipes.js
@@ -1,6 +1,28 @@
 // Remove IE recipes
 ServerEvents.recipes(event => {
-    
-    event.remove({id: "eternal_starlight:blast_furnace_from_cinder_brick"})
 
+    const remove_by_id = [
+        "eternal_starlight:blast_furnace_from_cinder_brick",
+    ]
+    remove_by_id.forEach(id => {
+        event.remove({ id: id })
+    });
+
+    global.dyeColors.forEach(color => {
+        yTechShaped(event, {
+            pattern: [
+                "# @",
+                "WWW",
+                "PPP"
+            ],
+            key: {
+                "#": { "tag": "c:hammers" },
+                "P": { "tag": "minecraft:wooden_slabs" },
+                "@": { "tag": "c:saws" },
+                "W": { "item": `eternal_starlight:${color.name}_yeti_fur` }
+            },
+            outputItems: [[{ id: `minecraft:${color.name}_bed` }, 1]],
+            removeRecipeType: "minecraft:crafting_shaped"
+        })
+    });
 })
