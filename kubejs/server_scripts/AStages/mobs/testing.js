a8ad73a754d469fef750647239b52a17535fff88
Kisuny <homeca19@gmail.com>
2025-11-24 21:07:35 +0300

    iron_ingot -> bronze_plate for mixing_cauldron

---
+++ kubejs/server_scripts/AStages/mobs/testing.js
@@ -0,0 +1,4 @@
+// AStages.addRestrictionForMob("skeleton_mob", "stage_mob", "minecraft:skeleton")
+//     .setEnableMobSpawning(true)
+//     .spawnReplacementWithEquipment(true)
+//     .setEquipment("chest", "minecraft:diamond_chestplate")
