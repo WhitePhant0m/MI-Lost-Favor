a9ef421debdb8f0abc05560eea01a2738c54b65b
Kisuny <homeca19@gmail.com>
2025-11-30 21:30:26 +0300

    Preparation for the Apotheosis balance

---
+++ kubejs/server_scripts/mods/apotheosis/apotheosis_recipes.js
@@ -0,0 +1,13 @@
+ServerEvents.recipes(event => {
+    event.remove({
+        output: [
+            'apotheosis:potion_charm',
+            'apothic_enchanting:flimsy_ender_lead',
+            'apothic_enchanting:ender_lead',
+            'apothic_enchanting:occult_ender_lead',
+        ]
+    })
+
+})
+
+
