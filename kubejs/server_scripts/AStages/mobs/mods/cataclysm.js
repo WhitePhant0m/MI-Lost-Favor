3d59e0a98657f8866fc8d95997d4dc5556615919
Kisuny <kitsunyme@gmail.com>
2026-01-28 23:11:04 +0300

    Removed some mobs from the spawn for correct mob progression.

--- kubejs/server_scripts/AStages/mobs/mods/cataclysm.js
+++ kubejs/server_scripts/AStages/mobs/mods/cataclysm.js
@@ -1,5 +1,7 @@
 const cataclysm_mobs = [
     {id:"koboleton", mob: "cataclysm:koboleton"},
+    {id:"deepling_angler", mob: "cataclysm:deepling_angler"},
+    {id:"lionfish", mob: "cataclysm:lionfish"},
 ]
 
 mobRestriction("cataclysm", cataclysm_mobs)
