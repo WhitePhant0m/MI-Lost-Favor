4ffb636dbba8c341adc2e97bcd9cfc9da16b302a
Kisuny <kitsunyme@gmail.com>
2026-01-28 23:15:10 +0300

    Changed recipes for weapons from the simplyswords mod to match modpack progression.

---
+++ kubejs/server_scripts/mods/simply_swords/simply_swords_recipes.js
@@ -0,0 +1,8 @@
+ServerEvents.recipes(event => {
+
+    event.replaceInput({mod: "simplyswords"}, "minecraft:diamond", "modern_industrialization:diamond_plate")
+    event.replaceInput({mod: "simplyswords"}, "minecraft:gold_ingot", "modern_industrialization:gold_plate")
+    event.replaceInput({mod: "simplyswords"}, "minecraft:iron_ingot", "modern_industrialization:iron_plate")
+    
+
+})
