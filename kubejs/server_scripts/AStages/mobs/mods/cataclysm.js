c86348363883c9dbb9d1a10f696406a10329e137
Kisuny <kitsunyme@gmail.com>
2026-01-29 19:13:04 +0300

    hide cataclysm mobs

--- kubejs/server_scripts/AStages/mobs/mods/cataclysm.js
+++ kubejs/server_scripts/AStages/mobs/mods/cataclysm.js
@@ -2,6 +2,8 @@ const cataclysm_mobs = [
     {id:"koboleton", mob: "cataclysm:koboleton"},
     {id:"deepling_angler", mob: "cataclysm:deepling_angler"},
     {id:"lionfish", mob: "cataclysm:lionfish"},
+    {id:"deepling_brute", mob: "cataclysm:deepling_brute"},
+    {id:"deepling", mob: "cataclysm:deepling"},
 ]
 
 mobRestriction("cataclysm", cataclysm_mobs)
