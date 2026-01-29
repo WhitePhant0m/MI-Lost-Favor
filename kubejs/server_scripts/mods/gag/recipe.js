535c08048093854781db09dd684541e2d4f94369
Kisuny <kitsunyme@gmail.com>
2026-01-29 19:12:24 +0300

    refactor for shrine_recipe and added recipe for time_sand_pouch

--- kubejs/server_scripts/mods/gag/recipe.js
+++ kubejs/server_scripts/mods/gag/recipe.js
@@ -1,7 +1,27 @@
 ServerEvents.recipes(event => {
 
-    event.remove({output: [
-        'gag:hearthstone',
-    ]})
+    event.remove({
+        output: [
+            'gag:hearthstone',
+        ]
+    })
+
+    customShrineRecipe(event, {
+        time: 24000,
+        experience: 16.0,
+        fluid: "pastel:liquid_crystal",
+        ingredients: [
+            { "item": "bosses_of_mass_destruction:blazing_eye", count: 1 },
+            { "item": "minecraft:bundle", count: 1 },
+            { "item": "minecraft:raw_gold_block", count: 8 },
+            { "item": "minecraft:nautilus_shell", count: 12 }
+        ],
+        result: {
+            "id": "gag:time_sand_pouch",
+            "count": 1
+        },
+        advancement: "pastel:midgame/enter_liquid_crystal"
+    })
+    event.remove({ output: 'gag:time_sand_pouch' })
 
 })
--- kubejs/server_scripts/mods/pastel/fusion_shrine.js
+++ kubejs/server_scripts/mods/pastel/fusion_shrine.js
@@ -1,51 +1,41 @@
+const customShrineRecipe = (event, args) => {
+    event.custom({
+        "type": "pastel:fusion_shrine",
+        "time": args.time,
+        "experience": args.experience || 0.0,
+        "fluid": {
+            "fluid": args.fluid
+        },
+        "copy_components": true,
+        "ingredients": args.ingredients,
+        "result": args.result,
+        "required_advancement": args.advancement || "pastel:unlocks/blocks/fusion_shrine",
+        "world_conditions": args.conditions || {},
+        "start_crafting_effect": args.startEffect || "nothing",
+        "during_crafting_effects": args.duringEffects || ["nothing", "nothing", "nothing"],
+        "finish_crafting_effect": args.finishEffect || "nothing",
+    });
+};
+
 ServerEvents.recipes(event => {
 
-
-    function shrine_recipe(time, fluid, items, output, advancement, conditions, startEffect, duringEffects, finishEffect){
-        advancement = advancement || "pastel:unlocks/blocks/fusion_shrine"
-        conditions = conditions || {}
-        startEffect = startEffect || "nothing"
-        duringEffects = duringEffects || ["nothing","nothing","nothing"]
-        finishEffect = finishEffect || "nothing"
-        let recipe = {
-            "type": "pastel:fusion_shrine",
-            "time": time,
-            "experience": 16.0,
-            "fluid": {
-                "fluid": fluid
-            },
-            "copy_components": true,
-            "ingredients": [],
-            "result": {
-                "id": output[0],
-                "count" : output[1]
-            },
-            "required_advancement": advancement,
-            "world_conditions": conditions,
-            "start_crafting_effect": startEffect,
-            "during_crafting_effects": duringEffects,
-            "finish_crafting_effect": finishEffect,
-        }
-        items.forEach(i => {
-            i[0].count = i[1]
-            recipe.ingredients.push(i[0])
-        });
-
-        event.custom(recipe)
-    }
-
-    shrine_recipe(60, "pastel:dragonrot",
-        [
-            [{ "item": "minecraft:amethyst_shard" }, 4],
-            [{ "item": "pastel:topaz_shard" }, 4],
-            [{ "item": "pastel:citrine_shard" }, 4],
-            [{ "item": "pastel:onyx_shard" }, 4],
-            [{ "item": "pastel:moonstone_shard" }, 4],
-            [{ "item": "ae2:certus_quartz_crystal" }, 4],
-            [{ "item": "pastel:downstone_fragments" }, 1],
-
+    customShrineRecipe(event, {
+        time: 60,
+        experience: 8.0,
+        fluid: "pastel:dragonrot",
+        ingredients: [
+            { "item": "minecraft:amethyst_shard", count: 4 },
+            { "item": "pastel:topaz_shard", count: 4 },
+            { "item": "pastel:citrine_shard", count: 4 },
+            { "item": "pastel:onyx_shard", count: 4 },
+            { "item": "pastel:moonstone_shard", count: 4 },
+            { "item": "ae2:certus_quartz_crystal", count: 4 },
+            { "item": "pastel:downstone_fragments", count: 1 },
         ],
-        ["extendedae:entro_shard", 1]
-    )
+        result: {
+            "id": "extendedae:entro_shard",
+            "count": 1
+        }
+    })
 
 })
