125022c7742e565e3079f2f02c6f889bdf4e312f
Kisuny <kitsunyme@gmail.com>
2026-02-15 10:50:59 +0300

    toolbelt recipes

--- kubejs/server_scripts/mods/tool_belts/upgrade.js
+++ kubejs/server_scripts/mods/tool_belts/upgrade.js
@@ -9,5 +9,35 @@ ServerEvents.recipes(event => {
         ],
         "result": {"count": 1, "id": "toolbelt:belt", "components":{"toolbelt:belt_size":4}}
     })
+
+    yTechShaped(event, {
+        pattern: [
+            "SLS",
+            "L#L",
+            "SSS"
+        ],
+        key: {
+            "#": { "tag": "ytech:bone_needles" },
+            "L": { "item": "minecraft:leather" },
+            "S": { "tag": "ytech:leather_strips" }
+        },
+        outputItems: [[{ id: "toolbelt:belt" }, 1]],
+        removeRecipeType: "minecraft:crafting_shaped"
+    })
+
+    yTechShaped(event, {
+        pattern: [
+            "S#S",
+            "SSS",
+            " L "
+        ],
+        key: {
+            "#": { "tag": "ytech:bone_needles" },
+            "L": { "item": "minecraft:leather" },
+            "S": { "tag": "ytech:leather_strips" }
+        },
+        outputItems: [[{ id: "toolbelt:pouch" }, 1]],
+        removeRecipeType: "minecraft:crafting_shaped"
+    })
 })
 
