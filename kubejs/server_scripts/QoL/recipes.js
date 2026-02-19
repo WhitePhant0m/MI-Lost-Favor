5729882424c03776788f7e753078147b579011d0
Kisuny <kitsunyme@gmail.com>
2026-02-19 17:30:40 +0300

    added treecap axe in early

---
+++ kubejs/server_scripts/QoL/recipes.js
@@ -0,0 +1,9 @@
+ServerEvents.recipes(event => {
+
+    event.shapeless(Item.of("minecraft:iron_axe", "[enchantments={levels:{\"apothic_enchanting:chainsaw\":1}}]"), [
+        "minecraft:iron_axe",
+        "paganbless:essence_of_the_forest"
+    ]);
+
+
+});
