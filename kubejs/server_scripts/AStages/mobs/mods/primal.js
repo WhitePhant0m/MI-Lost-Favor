1860a2acfd02f38e5be8bd1aaa7f4c6d976c6153
Kisuny <kitsunyme@gmail.com>
2026-03-02 18:11:56 +0300

    remove primal

--- kubejs/server_scripts/AStages/mobs/mods/primal.js
+++
@@ -1,9 +0,0 @@
-
-const primal_mobs = [
-    {id:"primal_shark", mob: "primal:shark"},
-    {id:"primal_bear", mob: "primal:bear"},
-    {id:"primal_crocodile", mob: "primal:crocodile"},
-    {id:"primal_eagle", mob: "primal:eagle"},
-]
-
-mobRestriction("primal", primal_mobs)
