a35c9ad5985948e4c8ff6043062a43c38d850b3c
Kisuny <kitsunyme@gmail.com>
2026-03-14 00:40:29 +0300

    new recipe for shrinking device

---
+++ kubejs/server_scripts/mods/shrink/shrink_recipes.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes(event => {
+    event.remove({output: "shrink:shrinking_device"})
+    event.recipes.modern_industrialization.assembler(8, 200)
+        .itemIn('3x modern_industrialization:analog_circuit')
+        .fluidIn('modern_industrialization:ethylene', 500)
+        .itemIn('9x modern_industrialization:aluminum_ingot')
+        .itemIn('ae2:quartz_vibrant_glass')
+        .itemOut('shrink:shrinking_device')
+        .id('milf:shrinking_device')
+
+});
