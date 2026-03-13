4eadb013303e76b95709100ff02cc53b13268618
Kisuny <kitsunyme@gmail.com>
2026-03-14 00:29:37 +0300

    new recipe for travel anchors

---
+++ kubejs/server_scripts/mods/travel_anchors/travel_anchors_recipes.js
@@ -0,0 +1,6 @@
+ServerEvents.recipes(event => {
+    event.replaceInput({mod: 'travelanchors'}, 'minecraft:ender_pearl', 'architects_palette:ender_pearl_block')
+    event.replaceInput({mod: 'travelanchors'}, 'minecraft:iron_ingot', 'modern_industrialization:steel_plate')
+    event.replaceInput({mod: 'travelanchors'}, 'minecraft:emerald', 'enchanted:attuned_stone')
+
+});
