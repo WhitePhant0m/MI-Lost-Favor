fb45d7a83daff0ce5b12e98f0930f85eacdde6ba
Kisuny <kitsunyme@gmail.com>
2026-03-22 10:00:36 +0300

    platform rename

--- kubejs/startup_scripts/Platform_rename.js
+++ kubejs/startup_scripts/Platform_rename.js
@@ -1,1 +1,7 @@
 Platform.mods.kubejs.name = 'MI:Lost Favor';
+Platform.getInfo("milf").name = "MI:Lost Favor";
+
+
+StartupEvents.modifyCreativeTab("kubejs:tab", (event) => {
+	event.displayName = "MI:Lost Favor";
+});
