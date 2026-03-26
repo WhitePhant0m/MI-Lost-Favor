0fe5e9f0eac2429b2df6e9d78b1093f80bab60fe
Kisuny <kitsunyme@gmail.com>
2026-03-26 18:42:33 +0300

    hexerei lootr compat

---
+++ kubejs/server_scripts/mods/lootr/lootr_tags.js
@@ -0,0 +1,8 @@
+ServerEvents.tags('block', event => {
+    event.add('lootr:convert/chests', 
+        [
+            'hexerei:willow_chest', 
+            'hexerei:witch_hazel_chest', 
+            'hexerei:mahogany_chest',
+        ])
+})
