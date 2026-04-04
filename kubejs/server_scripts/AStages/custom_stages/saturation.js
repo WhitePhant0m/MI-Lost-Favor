e6669717157b9bb7e23412c54033059c925c7c3b
Kisuny <kitsunyme@gmail.com>
2026-04-04 14:05:46 +0300

    remove infinity saturation on start

--- kubejs/server_scripts/AStages/custom_stages/saturation.js
+++ kubejs/server_scripts/AStages/custom_stages/saturation.js
@@ -1,11 +1,11 @@
-    PlayerEvents.tick(event => {
-    const { player } = event
-    if (player.tickCount % 20 != 0) return
-    if (AStages.playerHasStage("saturation", player)) {
-        player.removeEffect("minecraft:saturation")
-        return
-    } 
-    if (player.hasEffect('minecraft:saturation')) return
-    player.potionEffects.add("minecraft:saturation",-1)
+//     PlayerEvents.tick(event => {
+//     const { player } = event
+//     if (player.tickCount % 20 != 0) return
+//     if (AStages.playerHasStage("saturation", player)) {
+//         player.removeEffect("minecraft:saturation")
+//         return
+//     } 
+//     if (player.hasEffect('minecraft:saturation')) return
+//     player.potionEffects.add("minecraft:saturation",-1)
     
-})
+// })
