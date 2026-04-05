17a0e5846d5ebbe226e29e1d11dec864dfc2f9a2
Kisuny <kitsunyme@gmail.com>
2026-04-05 12:54:15 +0300

    magnumtorch recipes

---
+++ kubejs/server_scripts/mods/magnum_torch/magnum_torch_recipes.js
@@ -0,0 +1,7 @@
+ServerEvents.recipes(event => {
+    
+    // event.remove({output: [
+    // ]})
+    event.replaceInput( { input: 'minecraft:fire_charge', mod: 'magnumtorch' },'minecraft:fire_charge','hexerei:dried_sage_bundle')
+    
+})
