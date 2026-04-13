bdc57c10bdfe342a5ad10a50d10def301a6d7f3e
Kisuny <kitsunyme@gmail.com>
2026-04-13 15:57:05 +0300

    fix first join

--- kubejs/server_scripts/first_join.js
+++ kubejs/server_scripts/first_join.js
@@ -1,20 +1,17 @@
 PlayerEvents.loggedIn(event => {
     const { player, server } = event
-    if (player.persistentData.getBoolean('first_join')) return
+    if (!player || player.persistentData.getBoolean('first_join')) return
 
     player.persistentData.putBoolean('first_join', true)
 
-    server.scheduleInTicks(100, () => {
-        if (!player.isOnline()) return
-        player.inventory.clear()
-        player.give(Item.of('ftbquests:book', 1))
-        player.give(Item.of('minecraft:lantern', 1))
-        player.tell(Text.translate("milf.text.first_join"))
-        sendImmersiveMessage(
-            Text.translate('milf.text.first_join'),
-            event.player,
-            DEFAULT_MILESTONE_NOTIFICATION_STYLE,
-            event.server
-        )
-    })
+    player.inventory.clear()
+    player.give(Item.of('ftbquests:book', 1))
+    player.give(Item.of('minecraft:lantern', 1))
+    player.tell(Text.translate("milf.text.first_join"))
+    sendImmersiveMessage(
+        Text.translate('milf.text.first_join'),
+        event.player,
+        DEFAULT_MILESTONE_NOTIFICATION_STYLE,
+        event.server
+    )
 })
