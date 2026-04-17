0fe8c285866c4072baebdc02eb145c70580189ac
Kisuny <kitsunyme@gmail.com>
2026-04-17 10:59:13 +0300

    Removed old code

--- kubejs/server_scripts/AStages/mobs/testing.js
+++
@@ -1,4 +0,0 @@
-// AStages.addRestrictionForMob("skeleton_mob", "stage_mob", "minecraft:skeleton")
-//     .setEnableMobSpawning(true)
-//     .spawnReplacementWithEquipment(true)
-//     .setEquipment("chest", "minecraft:diamond_chestplate")
