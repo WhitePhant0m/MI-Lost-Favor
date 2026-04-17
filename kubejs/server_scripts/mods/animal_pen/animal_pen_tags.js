dd037e2ae0d40b31b9a5a07ff0c1fdaeb101c926
Kisuny <kitsunyme@gmail.com>
2026-04-17 11:16:46 +0300

    All weapons from simplyswords can now kill mobs in Animal Pens

---
+++ kubejs/server_scripts/mods/animal_pen/animal_pen_tags.js
@@ -0,0 +1,5 @@
+ServerEvents.tags("item", event => {
+    event.add("animal_pen:can_attack_aviary", "#simplyswords:swords")
+    event.add("animal_pen:can_attack_aquarium", "#simplyswords:swords")
+    event.add("animal_pen:can_attack_pen", "#simplyswords:swords")
+})
