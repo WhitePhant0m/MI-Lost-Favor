ace00a1325c6aa73d56eb8bebc69fe75200c3c62
Kisuny <kitsunyme@gmail.com>
2026-04-23 17:48:36 +0300

    New recipe for Natural Compass

--- kubejs/server_scripts/mods/naturescompass/naturescompass_recipes.js
+++ kubejs/server_scripts/mods/naturescompass/naturescompass_recipes.js
@@ -1,5 +1,19 @@
 ServerEvents.recipes(event => {
-    
-    event.remove({output: "naturescompass:naturescompass"})
-    
+        customWorktable(event, {
+        pattern: [
+            " r ", 
+            "ewe", 
+            " e "
+        ],
+        reagents: ["tyty"],
+        key: {
+            "w": { "item": "minecraft:compass" },
+            "e": { "item": "paganbless:black_thorn_log" },
+            "r": { "item": "hexerei:dowsing_rod" },
+            "t": { "item": "eidolon_repraised:mirecap" },
+            "y": { "item": "eidolon_repraised:avennian_sprig" },
+        },
+        result: "naturescompass:naturescompass",
+        removeRecipe: true
+    })
 })
